ce7e38277749ecea8a6a69075e988e06dde83a60
yoshitsugu <yoshitsugu@users.noreply.github.com>
2022-02-22 14:46:27 +0900

    Add FunctionSectionNode

--- lib/rwp/node/function_section_node.rb
+++ lib/rwp/node/function_section_node.rb
@@ -2,5 +2,16 @@
 
 module Rwp
   class FunctionSectionNode < SectionNode
+    # @dynamic type_idxs
+    attr_reader :type_idxs
+
+    def initialize
+      super
+      @type_idxs = []
+    end
+
+    def load(buffer)
+      @type_idxs = buffer.read_vec(&:read_u32)
+    end
   end
 end
